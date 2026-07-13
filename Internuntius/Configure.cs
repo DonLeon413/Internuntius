4ccb6fc6b97cd9f7e36cdb34455c6db52a486062
Don Leon <donleon314@gmail.com>
2026-07-14 00:19:52 +0200

    save

---
+++ Internuntius/Configure.cs
@@ -0,0 +1,19 @@
+﻿namespace Internuntius
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class Configure
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="thisObj"></param>
+        /// <returns></returns>
+        public static IServiceCollection ConfiguraApp( this IServiceCollection thisObj )
+        {
+            return thisObj;
+        }
+
+    }
+}
---
+++ Internuntius/Core/Entities/User.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Internuntius.Core.Entities
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class User
+    {
+        #region PROPERTIES
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonPropertyName( "Name" )]
+        public string Name
+        {
+            get;
+            init;
+        } = null!;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonPropertyName( "Description" )]
+        public string? Description
+        {
+            get;
+            init;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonPropertyName("Groups")]
+        public IEnumerable<string> Groups
+        {
+            get;
+            init;
+        } = Enumerable.Empty<string>();
+
+        #endregion
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public User()
+        { 
+        }
+    }
+}
